0e5322311db243860ba00b515a6a00635a7d2b10
eldoraboo <eldoraboo.mby@gmail.com>
2023-02-05 01:18:50 +0800

    feat: dark mode

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
     <React.StrictMode>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </React.StrictMode>
   </ChakraProvider>
